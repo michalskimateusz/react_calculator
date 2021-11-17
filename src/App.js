f0cf3f8633cdb59fb07b797734da1a32aaf977cb
Mateusz Michalski <michalskimateusz@outlook.com>
2021-11-17 22:07:53 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
     <div className="App">
       <div className="calculator">
         <div className="display">
-          { result ? <span>({result})</span> : ''}&nbsp;{calc || '0'}
+          { result ? <span>= {result}</span> : ''}&nbsp;{calc || '0'}
         </div>
         <div className="operators">
           <button onClick={()=> updateCalc('/')}>/</button>
@@ -58,8 +58,8 @@ function App() {
         </div>
         <div className="digits">
           { createDigits() }
-          <button onClick={()=> updateCalc('0')}>0</button>
           <button onClick={()=> updateCalc('.')}>.</button>
+          <button onClick={()=> updateCalc('0')}>0</button>
           <button onClick={calculate}>=</button>
         </div>
       </div>
